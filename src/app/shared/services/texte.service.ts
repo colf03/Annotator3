15e129e2c5b4dea3e6c304c75906d29e21dcfc5e
Unknown <steph.osbornes3@gmail.com>
2017-12-10 11:27:24 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ src/app/shared/services/texte.service.ts
@@ -0,0 +1,49 @@
+/*
+ *  Projet :  ANNOTATOR 3
+ *   Version du fichier : 1.0.0
+ *  Fichier : annotation.service.ts
+ *  Application de gestion des annotations - Description
+ *  Date :04-12-2017,  
+ *  Crée par : Bastien SANSAS
+ */
+
+import { Annotation } from '../models';
+
+/*@Injectable()*/
+export class AnnotationService {
+  constructor (
+  ) {}
+/*
+  getAnnotations(idTexte): Observable<Annotation> {
+    
+  }
+
+  getCategories(idProjet): Observable<string> {
+    
+  }
+	
+  ajouterAnnotation(newAnnotation): boolean {
+    return true;
+  }
+  
+  supprimerAnnotation(idAnnotation): boolean {
+    return true;
+  }
+  
+  modifierAnnotation(AnnotationAModifer ): boolean {
+    return true;
+  }
+  
+  ajouterCategorie(newCategorie) : boolean {
+    return true;
+  }
+  
+  supprimerCategorie(CategorieASupprimer) : boolean {
+    return true;
+  }
+  
+  supprimerCategorie(CategorieAModifer) : boolean {
+    return true;
+  }
+*/
+}
